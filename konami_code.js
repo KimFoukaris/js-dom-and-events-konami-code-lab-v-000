8f663a31d3d6c7ec0a02d3b6f00780c73e3a04a3
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-05 18:04:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  write("Hello!")
+  write("Hello!");
   document.body.addEventListener('keydown', onKeyDownHandler);
 }
 
