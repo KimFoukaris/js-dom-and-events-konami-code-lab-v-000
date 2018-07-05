03a10f0289d6665d3fc9668c940d00b87dcb53ee
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-05 17:57:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,20 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  const input = document.querySelector('input')
+  document.body.addEventListener('keydown', onKeyDownHandler(e));
+}
 
-  input.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-
-    if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
       index = 0;
-    })
+    }
+  } else {
+    index = 0;
+  }
 }
